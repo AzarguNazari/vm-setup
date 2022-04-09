8bbf8d3d7427e7a95d65ecb6d3c9989e2736b6dc
Hazar Gul Nazari <nazariazargul@gmail.com>
2022-04-09 12:47:02 +0200

    add scripts

--- setup.sh
+++ setup.sh
@@ -3,6 +3,19 @@
 sudo apt update
 sudo apt upgrade -y
 
+# install git
+sudo apt install git
+
+# setup git
 ssh-keygen -o -t rsa -C "email@email.com"
+
+# install google chrome 
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get update
+sudo apt-get install google-chrome-stabl
+
+# install vscode
+sudo snap install --classic code
 
 
